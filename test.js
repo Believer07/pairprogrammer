bc9136a87cae121b1a11d2112599d4d9452cd50c
Harsh Hingorani <50800669+Believer07@users.noreply.github.com>
2025-10-07 22:36:22 +0530

    chore: add/update test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+File: test.js
+// Sample JSON object added for demonstration.
+const sampleObject = {
+    "name": "Sample Name",
+    "age": 30,
+    "city": "Sample City"
+};
+
+// The code above can be used for testing purposes.
